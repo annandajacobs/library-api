7d4474604066f41441eebe1a5d9cb40b7376b5cf
annandajacobs <annandajacobs@gmail.com>
2026-02-05 17:24:10 -0300

    primeiro commit

---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreAuthorRequest;
+use App\Http\Resources\AuthorResource;
+use App\Models\Author;
+use Illuminate\Http\Request;
+
+class AuthorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $authors = Author::paginate(10);
+
+        return response()->json([
+            'authors' => $authors,
+            'message' => 'Autores encontrados com sucesso!'
+        ], 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreAuthorRequest $request)
+    {
+        $author = Author::create($request->validated());
+
+        return new AuthorResource($author);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
